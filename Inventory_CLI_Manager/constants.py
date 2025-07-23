8c60b3e7f0cdc607ecb9c5d0d5fc6fd61c0fffc6
MUHAMMAD IDREES <midrees4040@gmail.com>
2025-07-23 16:37:53 +0500

    Some updates were done inside 'inventory.py', 'constants.py' and modified a structure to moved flowchart images from brainstorm_idea to assets folder

--- Inventory_CLI_Manager/constants.py
+++ Inventory_CLI_Manager/constants.py
@@ -6,35 +6,3 @@ MENU = """
 2. Remove Product
 3. Show All Products
 4. Exit"""
-
-
-DEFAULT_PRODUCTS = [
-    {
-        "name": "Laptop",
-        "price": 75000
-    },
-    {
-        "name": "Keyboard",
-        "price": 3200
-    },
-    {
-        "name": "Mouse",
-        "price": 5000
-    },
-    {
-        "name": "Air Pods",
-        "price": 5700
-    },
-    {
-        "name": "Oppo A37",
-        "price": 47000
-    },
-    {
-        "name": "Reno G10",
-        "price": 62999
-    },
-    {
-        "name": "iPhone 16 Pro Max",
-        "price": 462999
-    },
-]
--- Inventory_CLI_Manager/inventory.py
+++ Inventory_CLI_Manager/inventory.py
@@ -1,67 +1,65 @@
 # Will now depend on file_handler to read/write JSON
-# Remove DEFAULTS_PRODUCTS global
-
 # Core logic: add, remove, display products
 
 # ############ Import modules & packages #############
-from constants import DEFAULT_PRODUCTS
 from utils import is_valid_product_name, is_valid_product_price, process_control_user_choice
 
 
-def add_product(p_name, p_price):
+def add_product(products, p_name, p_price):
     """Add products to the inventory list in the form of dict"""
     
     p_name = (is_valid_product_name(p_name)).strip().lower()
     p_price = float(is_valid_product_price(p_price))
     
-    if not any(p["name"].lower() == p_name.lower() for p in DEFAULT_PRODUCTS):
-        DEFAULT_PRODUCTS.append({"name": p_name.strip().lower(), "price": p_price})
+    if not any(p["name"].lower() == p_name.lower() for p in products):
+        products.append({"name": p_name.strip().lower(), "price": p_price})
         print(f"\nProduct '{p_name}' added successfully!".title())
     else:
         print(f"⚠️  Product {p_name} already exists! Use a differet name.\n")
 
 
-def upsert_product():
+def upsert_product(products):
     """If product not exists during remove_product_process or inventory empty, then add?"""
     
     product_add_user_choice = input(f"Add new product? (Y/N): ".title())
     if process_control_user_choice(product_add_user_choice):
         p_name = input("Enter product name: ")
         p_price = input("Enter product price: ")
-        add_product(p_name, p_price)
+        add_product(products, p_name, p_price)
 
-def rem_product(p_name):
+
+def rem_product(products, p_name):
     """Remove products from inventory"""
-    global DEFAULT_PRODUCTS
+    
     p_name = (is_valid_product_name(p_name)).strip().lower()
     
-    if len(DEFAULT_PRODUCTS) == 0:
+    if len(products) == 0:
         print("❌  Error! Prodcut inventory is empty!\n")
-        upsert_product()    # if user want to add new product
+        upsert_product(products)    # if user want to add new product
         return
     
     # Changing a global variable like below will cause an error due scope. So, we've used 'global' keyword
     
     try:
-        if any(p["name"].lower() == p_name for p in DEFAULT_PRODUCTS):
-            DEFAULT_PRODUCTS[:] = [p for p in DEFAULT_PRODUCTS if p["name"].lower() != p_name.lower()]
+        if any(p["name"].lower() == p_name for p in products):
+            products[:] = [p for p in products if p["name"].lower() != p_name.lower()]
             print(f"\nProduct '{p_name}' removed successfully!".title())
             return
     except (ValueError, FileNotFoundError) as e:
         print(f"❌  Error: {e}\n")
     
     print(f"⚠️  Error! Product '{p_name}' doesn't exists.")
-    upsert_product()    # if user want to add new product
+    upsert_product(products)    # if user want to add new product
     return
 
 
-def list_products():
+def list_products(products):
     """Show all inventory products to user"""
     
-    if len(DEFAULT_PRODUCTS) == 0:
+    if len(products) == 0:
         print("❌  Error! Product inventory is empty!\n")
-        upsert_product()    # if user want to add new product
+        upsert_product(products)    # if user want to add new product
         return
     
-    for idx, product in enumerate(DEFAULT_PRODUCTS, 1):
+    for idx, product in enumerate(products, 1):
         print(f"{idx:>2}. {product['name']:<20} ${product['price']:>10,.2f}")   # little bit formatted
